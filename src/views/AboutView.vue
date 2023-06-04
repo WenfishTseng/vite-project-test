e9d45a8b32af39051f1c9ebc27c50553e20d6774
WenfishTseng <twy0171@outlook.com>
2023-06-04 20:56:14 +0800

    first add

---
+++ src/views/AboutView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="about">
+    <ul class="">
+      <li v-for="item in myData" :key="item.id.value"> 
+        <img :src="item.picture.medium" alt="">
+        <p> {{ item.name.last }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      myData:{}
+    }
+  },
+  async mounted() {
+    console.log(import.meta.env.VITE_TEXT);
+    await this.$http.get(import.meta.env.VITE_PATH).then((res) => {
+     
+      this.myData = res.data.results;
+      console.log(this.myData);
+    })
+  }
+}
+</script>
+
+<style>
+ul {
+  list-style:none
+}
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
